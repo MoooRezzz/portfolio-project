91af470033ffdbb1e9e06c3f66a38e7bfd2f04aa
mohammadreza <rezaghaderi10@gmail.com>
2020-05-05 02:09:46 +0430

    Until adding BlogApp

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
